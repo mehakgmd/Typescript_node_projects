43f955325a5580ebbfe2d5c566d818a2b3aee331
mehakgmd <115011143+mehakgmd@users.noreply.github.com>
2023-01-09 20:08:53 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+(
+    async () => {
+        const userInput: {userId : string, userPin : number} = await inquirer.prompt([
+            {
+                name : "userId",
+                message : "Enter your user Id",
+                type : "input"
+            },
+            {
+                name : "userPin",
+                message : "Enter your pin",
+                type : "password"
+            }
+        ])
+        console.log("User Id",userInput.userId)
+        console.log("User Pin",userInput.userPin)
+        const userData = {
+            userId : userInput.userId,
+            userPin : userInput.userPin,
+            amount : Math.floor(Math.random()*100000 + 1)
+        }
+        console.log(userData)
+        const selectedOpt: {options : "cash withdrawl" | "exit"} = await inquirer.prompt([
+            {
+                name: "options",
+                message: "Select Any Options",
+                type: "list",
+                choices : ["cash withdrawl" , "exit"]
+            }
+ 
+        ])
+        if (selectedOpt.options === "cash withdrawl") {
+            console.log("Your current amount:",userData.amount)
+            const enterAmount:{amount : number } = await inquirer.prompt([
+                {
+                    message : "Enter your amount",
+                    name : "amount",
+                    type : "number",
+                    validate : (input) => {
+                        if(input > userData.amount){
+                            return "Insufficient Balance"
+                        }
+                        else {
+                            return true
+                        }
+                    }
+                }
+            ])
+            userData.amount -= enterAmount.amount
+            console.log ("Amount after withdrawl:" , userData.amount)
+        }
+    }
+)()
+export{};
+
+
